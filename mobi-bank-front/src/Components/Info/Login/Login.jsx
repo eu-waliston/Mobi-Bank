20a9f3bb50097ae0dca67cb4d6ff7692ce298224
wal-wizard <goneisgone@hotmail.com>
2023-09-24 19:17:52 -0300

    update

--- mobi-bank-front/src/Components/Info/Login/Login.jsx
+++ mobi-bank-front/src/Components/Info/Login/Login.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import "./Login.scss";
 
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [cpf, setCpf] = React.useState();
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) =>{
+    setCpf(data)
+  }
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("example"));
+
+  const handleClick = () => {
+    if(cpf !== undefined ) {
+      navigate("/bank/home/")
+    }
+  }
 
   return (
     <div className="Login__Component">
@@ -39,7 +50,7 @@ const Login = () => {
             <span className="warn">This field is required</span>
           )}
           <br />
-          <button type="submit">Login</button>
+          <button type="submit" onClick={() => handleClick()}>Login</button>
           <p>
             don't have an account ?{" "}
             <Link to={"/bank/register/"} className="reg">
